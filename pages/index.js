7ffb9ef3ac2b3c228e28fb6118fb5dd9d8240e0b
biarobotics <carl@biarobotics.com>
2025-10-20 18:59:57 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,13 @@
 // pages/index.js
-export default function Home() {
-  // This component never renders because we redirect on the server.
-  return null;
-}
-
 export async function getServerSideProps() {
-  // Redirect the root (/) to your static landing page
   return {
     redirect: {
       destination: '/index.html',
       permanent: false,
     },
   };
+}
+
+export default function Home() {
+  return null;
 }
